b4dc4ea5e53974d76e287023561904cac7f78fe9
Sofia Nguy <sofianguy@gmail.com>
2016-07-11 16:14:30 -0700

    fulfill a promise exercise

---
+++ 02-fulfill-a-promise.js
@@ -0,0 +1,17 @@
+// Create a promise. Have it fulfilled with a value 
+// of 'FULFILLED!' in executor after a delay of 300ms, 
+// using setTimeout.
+
+// Then, print the contents of the promise after 
+// if has been fulfilled by passing console.log to then.
+
+'use strict';
+
+var promise = new Promise(function (fulfill, reject) {
+  // Your solution here
+  setTimeout(function() {
+		fulfill('FULFILLED!');
+	}, 300);
+});
+
+promise.then(console.log);
